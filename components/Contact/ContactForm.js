584a3d5cd3d6a725cf21d462e9a608c4531b910a
Sopan Ganage <gangesm1@gmail.com>
2023-07-15 13:54:25 +0530

    updating contact api

--- components/Contact/ContactForm.js
+++ components/Contact/ContactForm.js
@@ -31,10 +31,11 @@ const ContactForm = () => {
   const handleChange = (e) => {
     const { name, value } = e.target;
     setContact((prevState) => ({ ...prevState, [name]: value }));
-    // console.log(contact)
+    console.log(contact)
   };
 
   const handleSubmit = async (e) => {
+    debugger;
     e.preventDefault();
     try {
       const url = `${baseUrl}/api/contact`;
