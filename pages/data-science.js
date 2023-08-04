bab2d55afc02d15e0ca0b06ba9b5c035c31c8512
Sopan Ganage <gangesm1@gmail.com>
2023-08-04 22:57:44 +0530

    add Data Science page

---
+++ pages/data-science.js
@@ -0,0 +1,26 @@
+import Navbar from "../components/Live/Navbar";
+import Footer from "../components/Live/Footer";
+import HeroBanner from "../components/data-science/HeroBanner";
+import Industries from "../components/data-science/Industries";
+import Testimonials from "../components/data-science/Testimonials";
+import PartnerSlider from "../components/data-science/PartnerSlider";
+import WhyTriosource from "../components/data-science/WhyTriosource";
+import ComputerVisionAI from "../components/data-science/ComputerVisionAI";
+import TechDetailsContent from "../components/data-science/TechDetailsContent";
+
+const Index = () => {
+  return (
+    <>
+      <Navbar />
+      <HeroBanner />
+      <ComputerVisionAI />
+      <TechDetailsContent />
+      <Industries />
+      <WhyTriosource />
+      <PartnerSlider />
+      <Testimonials />
+      <Footer />
+    </>
+  )
+}
+export default Index;
